2ce3b6392759493142c56943bee45a84988576e5
Your Name <your-email@example.com>
2026-01-15 22:50:25 +0100

    EMERGENCY: Update Firebase to V2-compatible versions (core ^4.3.0, auth ^6.1.3)

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -66,7 +66,7 @@ import 'app_localizations_yo.dart';
 /// property.
 abstract class AppLocalizations {
   AppLocalizations(String locale)
-    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -89,11 +89,11 @@ abstract class AppLocalizations {
   /// of delegates is preferred or required.
   static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
       <LocalizationsDelegate<dynamic>>[
-        delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ];
+    delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
@@ -101,13 +101,13 @@ abstract class AppLocalizations {
     Locale('ha'),
     Locale('ig'),
     Locale('pcm'),
-    Locale('yo'),
+    Locale('yo')
   ];
 
   /// No description provided for @appTitle.
   ///
   /// In en, this message translates to:
-  /// **'Nexus Fertility'**
+  /// **'Nexus Fertility App'**
   String get appTitle;
 
   /// No description provided for @welcome.
@@ -115,288 +115,6 @@ abstract class AppLocalizations {
   /// In en, this message translates to:
   /// **'Welcome'**
   String get welcome;
-
-  /// No description provided for @home.
-  ///
-  /// In en, this message translates to:
-  /// **'Home'**
-  String get home;
-
-  /// No description provided for @settings.
-  ///
-  /// In en, this message translates to:
-  /// **'Settings'**
-  String get settings;
-
-  /// No description provided for @language.
-  ///
-  /// In en, this message translates to:
-  /// **'Language'**
-  String get language;
-
-  /// No description provided for @english.
-  ///
-  /// In en, this message translates to:
-  /// **'English'**
-  String get english;
-
-  /// No description provided for @yoruba.
-  ///
-  /// In en, this message translates to:
-  /// **'Yoruba'**
-  String get yoruba;
-
-  /// No description provided for @igbo.
-  ///
-  /// In en, this message translates to:
-  /// **'Igbo'**
-  String get igbo;
-
-  /// No description provided for @hausa.
-  ///
-  /// In en, this message translates to:
-  /// **'Hausa'**
-  String get hausa;
-
-  /// No description provided for @pidgin.
-  ///
-  /// In en, this message translates to:
-  /// **'Pidgin'**
-  String get pidgin;
-
-  /// No description provided for @selectLanguage.
-  ///
-  /// In en, this message translates to:
-  /// **'Select Language'**
-  String get selectLanguage;
-
-  /// No description provided for @next.
-  ///
-  /// In en, this message translates to:
-  /// **'Next'**
-  String get next;
-
-  /// No description provided for @back.
-  ///
-  /// In en, this message translates to:
-  /// **'Back'**
-  String get back;
-
-  /// No description provided for @skip.
-  ///
-  /// In en, this message translates to:
-  /// **'Skip'**
-  String get skip;
-
-  /// No description provided for @done.
-  ///
-  /// In en, this message translates to:
-  /// **'Done'**
-  String get done;
-
-  /// No description provided for @createAccount.
-  ///
-  /// In en, this message translates to:
-  /// **'Create Account'**
-  String get createAccount;
-
-  /// No description provided for @signIn.
-  ///
-  /// In en, this message translates to:
-  /// **'Sign In'**
-  String get signIn;
-
-  /// No description provided for @email.
-  ///
-  /// In en, this message translates to:
-  /// **'Email'**
-  String get email;
-
-  /// No description provided for @phoneNumber.
-  ///
-  /// In en, this message translates to:
-  /// **'Phone Number'**
-  String get phoneNumber;
-
-  /// No description provided for @password.
-  ///
-  /// In en, this message translates to:
-  /// **'Password'**
-  String get password;
-
-  /// No description provided for @confirmPassword.
-  ///
-  /// In en, this message translates to:
-  /// **'Confirm Password'**
-  String get confirmPassword;
-
-  /// No description provided for @firstName.
-  ///
-  /// In en, this message translates to:
-  /// **'First Name'**
-  String get firstName;
-
-  /// No description provided for @lastName.
-  ///
-  /// In en, this message translates to:
-  /// **'Last Name'**
-  String get lastName;
-
-  /// No description provided for @profile.
-  ///
-  /// In en, this message translates to:
-  /// **'Profile'**
-  String get profile;
-
-  /// No description provided for @logout.
-  ///
-  /// In en, this message translates to:
-  /// **'Logout'**
-  String get logout;
-
-  /// No description provided for @save.
-  ///
-  /// In en, this message translates to:
-  /// **'Save'**
-  String get save;
-
-  /// No description provided for @cancel.
-  ///
-  /// In en, this message translates to:
-  /// **'Cancel'**
-  String get cancel;
-
-  /// No description provided for @ok.
-  ///
-  /// In en, this message translates to:
-  /// **'OK'**
-  String get ok;
-
-  /// No description provided for @error.
-  ///
-  /// In en, this message translates to:
-  /// **'Error'**
-  String get error;
-
-  /// No description provided for @success.
-  ///
-  /// In en, this message translates to:
-  /// **'Success'**
-  String get success;
-
-  /// No description provided for @loading.
-  ///
-  /// In en, this message translates to:
-  /// **'Loading...'**
-  String get loading;
-
-  /// No description provided for @tryAgain.
-  ///
-  /// In en, this message translates to:
-  /// **'Try Again'**
-  String get tryAgain;
-
-  /// No description provided for @selectOption.
-  ///
-  /// In en, this message translates to:
-  /// **'Please select an option'**
-  String get selectOption;
-
-  /// No description provided for @verifyPhoneTitle.
-  ///
-  /// In en, this message translates to:
-  /// **'Verify Phone Number'**
-  String get verifyPhoneTitle;
-
-  /// Message indicating a code was sent
-  ///
-  /// In en, this message translates to:
-  /// **'A verification code has been sent to {phone}'**
-  String codeSentToPhone(String phone);
-
-  /// Message showing code expiration time
-  ///
-  /// In en, this message translates to:
-  /// **'Code expires in {minutes} minutes'**
-  String codeExpiresIn(int minutes);
-
-  /// No description provided for @verify.
-  ///
-  /// In en, this message translates to:
-  /// **'Verify'**
-  String get verify;
-
-  /// No description provided for @didntReceiveCode.
-  ///
-  /// In en, this message translates to:
-  /// **'Didn\'t receive the code?'**
-  String get didntReceiveCode;
-
-  /// No description provided for @choosePreferredMethod.
-  ///
-  /// In en, this message translates to:
-  /// **'Choose your preferred signup method'**
-  String get choosePreferredMethod;
-
-  /// No description provided for @createAccountWithEmail.
-  ///
-  /// In en, this message translates to:
-  /// **'Create account with Email'**
-  String get createAccountWithEmail;
-
-  /// No description provided for @createAccountWithPhone.
-  ///
-  /// In en, this message translates to:
-  /// **'Create account with Phone'**
-  String get createAccountWithPhone;
-
-  /// No description provided for @selectDate.
-  ///
-  /// In en, this message translates to:
-  /// **'Select Date'**
-  String get selectDate;
-
-  /// No description provided for @continueText.
-  ///
-  /// In en, this message translates to:
-  /// **'Continue'**
-  String get continueText;
-
-  /// No description provided for @profileSetupComplete.
-  ///
-  /// In en, this message translates to:
-  /// **'Profile setup complete!'**
-  String get profileSetupComplete;
-
-  /// No description provided for @supportHub.
-  ///
-  /// In en, this message translates to:
-  /// **'Support Hub'**
-  String get supportHub;
-
-  /// No description provided for @dailyAffirmation.
-  ///
-  /// In en, this message translates to:
-  /// **'Daily Affirmation'**
-  String get dailyAffirmation;
-
-  /// No description provided for @stayPositive.
-  ///
-  /// In en, this message translates to:
-  /// **'Stay positive and believe in yourself'**
-  String get stayPositive;
-
-  /// No description provided for @exploreCommunityGroups.
-  ///
-  /// In en, this message translates to:
-  /// **'Explore Community Groups'**
-  String get exploreCommunityGroups;
-
-  /// No description provided for @chooseSupportMode.
-  ///
-  /// In en, this message translates to:
-  /// **'Choose Your Support Mode'**
-  String get chooseSupportMode;
 }
 
 class _AppLocalizationsDelegate
@@ -432,9 +150,8 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-    'an issue with the localizations generation tool. Please file an issue '
-    'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.',
-  );
+      'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+      'an issue with the localizations generation tool. Please file an issue '
+      'on GitHub with a reproducible sample app and the gen-l10n configuration '
+      'that was used.');
 }
