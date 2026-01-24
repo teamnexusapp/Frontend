02872a20cf6c720ab359e8d8e26c3519a75f542b
Sandra Aframea Ofosu <168097776+Aframia@users.noreply.github.com>
2026-01-24 13:56:13 +0000

    Fixed dependencies build issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 ﻿import "package:flutter/material.dart";
+import "package:flutter_localizations/flutter_localizations.dart";
 import "screens/calendar_screen.dart";
 import "theme/app_theme.dart";
+import "flutter_gen/gen_l10n/app_localizations.dart";
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +20,19 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       home: const CalendarScreen(),
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ha'),
+        Locale('ig'),
+        Locale('pt'),
+        Locale('yo'),
+      ],
     );
   }
 }
